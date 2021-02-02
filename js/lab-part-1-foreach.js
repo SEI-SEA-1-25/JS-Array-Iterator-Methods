07b39f8d05e9ef6077ca652b9e38bebf52a3659b
wonkatin <j.drawers@gmail.com>
2021-02-02 15:15:58 -0600

    Initial Commit: Lab work

--- js/lab-part-1-foreach.js
+++ js/lab-part-1-foreach.js
@@ -4,11 +4,12 @@
 // > "I like pizza"
 // > "I like tacos"
 // > "I like ice cream"
- 
-const foods = ["pizza", "tacos", "ice cream", "sushi"];
 
 // your code here
-
+const foods = ["pizza", "tacos", "ice cream", "sushi"];
+foods.forEach(function (x) {
+  console.log(`I like ${x}`);
+});
 // (2) Use the .forEach iterator to loop over the following array of objects and say how delicious each one is.
 // The output should be
 // > Pizza is very delicious
@@ -16,9 +17,11 @@ const foods = ["pizza", "tacos", "ice cream", "sushi"];
 // > Cottage Cheese is not very delicious
 
 const foodLevels = [
-  {name: "Pizza", level: "very"},
-  {name: "Tacos", level: "mostly"},
-  {name: "Cottage Cheese", level: "not very"}
+  { name: "Pizza", level: "very" },
+  { name: "Tacos", level: "mostly" },
+  { name: "Cottage Cheese", level: "not very" },
 ];
-
+foodLevels.forEach(function (x) {
+  console.log(`${x.name} is ${x.level} delicious`);
+});
 // your code here
